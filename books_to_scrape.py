6d0a2ec01c480bba0367c5007b5f24c285e99067
Jose Luis Lopez Lopez Prieto <ing.jlllopezp@gmail.com>
2022-09-15 01:58:05 -0500

    Esta version puede descargar los datos de la categoría Biography

--- books_to_scrape.py
+++ books_to_scrape.py
@@ -5,16 +5,12 @@ from scrapy.loader.processors import MapCompose
 from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
 from scrapy.crawler import CrawlerProcess
+import re
 
-
-# categoria = str(input('Escriba una categoria a scrapear: ')).lower()
+categorias_a_buscar = ['Biography']
+categorias_a_buscar.append(input('Ingresa una categoría a buscar: ').capitalize())
 
 class Book(Item):
-    """Esta clase contiene la información de los campos 
-
-    Args:
-        Item (_type_): _description_
-    """
     titulo = Field()
     categoria = Field()
     upc = Field()
@@ -22,37 +18,34 @@ class Book(Item):
     stock = Field()
     rating = Field()
 
-class BooksToScrape(CrawlSpider):
-    name = 'BooksFromBooksToScrape'
+class BookSpider(CrawlSpider):
+    name = 'books'
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/80.0.3987.132 Safari/537.36',
 
-        'CLOSESPIDER_PAGECOUNT': 50
+        'CLOSESPIDER_PAGECOUNT': 100
     }
     allowed_domains = ['toscrape.com']
     start_urls = ['http://books.toscrape.com/']
     download_delay = 1
 
     rules = (
-        # Biography category
         Rule(
             LinkExtractor(allow=r'/biography_\d+/'),
-            follow=True
-        ),
-
-        Rule(
-            LinkExtractor(
-                allow=r'/catalogue/',
-                restrict_xpaths=['//ol']),
             follow=True,
-            callback='parse_book'
-        ),
+            callback='ingresar_a_libros'),
     )
 
+    def ingresar_a_libros(self, response):
+        links_libros_en_categoria = response.xpath('//ol//li//h3//a')
+        for libro in links_libros_en_categoria:
+            yield response.follow(libro, callback =self.parsear_libro)
+
     def obtener_stock(self, texto):
-        return str(texto)
+        pattern = re.findall(r'\d+', texto)
+        return pattern
 
     def obtener_calificacion(self, texto):
         ratings = {
@@ -77,8 +70,6 @@ class BooksToScrape(CrawlSpider):
         item.add_xpath('precio','(//p[contains(@class,"price")])[1]/text()', MapCompose(self.limpiar_precio))
         item.add_xpath('stock','(//div[contains(@class,"main")]//p[contains(@class,"stock")])[1]/text()', MapCompose(self.obtener_stock))
         item.add_xpath('rating','//div[contains(@class,"main")]//p[contains(@class,"star-rating")]/@class', MapCompose(self.obtener_calificacion))
-
-
         yield item.load_item()
 
 
@@ -90,5 +81,5 @@ process = CrawlerProcess(settings={
               },
 })
 
-process.crawl(BooksToScrape)
+process.crawl(BookSpider)
 process.start() # the script will block here until the crawling is finished
